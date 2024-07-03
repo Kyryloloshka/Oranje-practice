db85de3d807e62bd49d5113297746db39ecac4f6
Kyryloloshka <kirill.contrmptuous@gmail.com>
2024-07-03 18:53:54 +0300

    add login

--- api/Controllers/UserController.cs
+++ api/Controllers/UserController.cs
@@ -9,6 +9,7 @@ using api.Mappers;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -18,8 +19,10 @@ namespace api.Controllers
     {
         private readonly UserManager<User> _userManager;
         private readonly ITokenService _tokenService;
-        public UserController(UserManager<User> userManager, ITokenService tokenService)
+        private readonly SignInManager<User> _signInManager;
+        public UserController(UserManager<User> userManager, ITokenService tokenService, SignInManager<User> signInManager)
         {
+            _signInManager = signInManager;
             _userManager = userManager;
             _tokenService = tokenService;
         }
@@ -56,6 +59,40 @@ namespace api.Controllers
                         UserName = appUser.UserName, 
                         Email = appUser.Email,
                         Token = _tokenService.CreateToken(appUser)
+                    }
+                );
+            }
+            catch (System.Exception e)
+            {
+                return StatusCode(500, e);
+            }
+        }
+    
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+            
+                var user = await _userManager.Users.FirstOrDefaultAsync(x => x.Email == loginDto.Email.ToLower());
+                if (user == null)
+                {
+                    return Unauthorized("Invalid email");
+                }
+
+                var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+                if (!result.Succeeded)
+                {
+                    return Unauthorized("Email or password is incorrect");
+                }
+
+                return Ok(
+                    new NewUserDto{
+                        UserName = user.UserName, 
+                        Email = user.Email,
+                        Token = _tokenService.CreateToken(user)
                     }
                 );
             }
